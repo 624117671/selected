875cda58ef50346b2e67fa26d521b4b69d26662b
624117671 <624117671@qq.com>
2018-11-15 17:58:25 +0800

    三级联动

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,9 +10,9 @@ export default {
     data(){
         return{
           url:{
-            province:'http://202.108.98.3/JmProject_Left-Behind_Children/Children/getCountByCivil?civilregionalismcode=111111',
-            city:'http://202.108.98.3/JmProject_Left-Behind_Children/Civilregionalism/getCity',
-            area:'http://202.108.98.3/JmProject_Left-Behind_Children/Civilregionalism/getCountry'
+            province:'',
+            city:'',
+            area:''
           },
           province:"113938e7ef1b4c6089d098afb721edf3",
           city:"c45e3feeacbc4d0a8f2a49d012edc813",
